0c1d5614eb2fb5dc0a5cb0d35e0e6ec936129053
Mikhail Agapov <mikhail.agapov@decentraland.org>
2026-03-12 16:20:04 +0300

    Benchmark Linear Scan vs Concurrent Dictionary
    Signed-off-by: Mikhail Agapov <mikhail.agapov@decentraland.org>

--- src/DCLPulse/InterestManagement/SpatialGrid.cs
+++ src/DCLPulse/InterestManagement/SpatialGrid.cs
@@ -1,19 +1,19 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using Pulse.Peers;
-using System.Collections.Concurrent;
 
 namespace Pulse.InterestManagement;
 
 public sealed class SpatialGrid(float cellSize, int maxPeers)
 {
+    // Sentinel: long.MinValue == PackKey(int.MinValue, 0), mapping to ~1.07B world units from origin.
     // No playable area can reach that coordinate.
-    private const long NO_CELL = long.MinValue;
+    public const long NO_CELL = long.MinValue;
 
     private readonly float inverseCellSize = 1f / cellSize;
-
-    private readonly ConcurrentDictionary<long, HashSet<PeerIndex>> cells = new ();
     private readonly long[] peerCellKeys = InitKeys(maxPeers);
+
+    public int MaxPeers => peerCellKeys.Length;
 
     private static long[] InitKeys(int count)
     {
@@ -22,41 +22,19 @@ public sealed class SpatialGrid(float cellSize, int maxPeers)
         return keys;
     }
 
-    public void Set(PeerIndex peer, Vector3 position)
-    {
-        long key = ComputeKey(position);
+    public void Set(PeerIndex peer, Vector3 position) =>
+        Volatile.Write(ref peerCellKeys[peer.Value], ComputeKey(position));
 
-        if (!cells.ContainsKey(key))
-            cells.TryAdd(key, new HashSet<PeerIndex>());
-
-        // No need to move the peer if already exists in the same cell
-        if (!cells[key].Add(peer)) return;
-
-        // Move the peer into the new cell
-        long prevKey = Volatile.Read(ref peerCellKeys[peer.Value]);
-
-        if (prevKey != NO_CELL && prevKey != key)
-            cells[prevKey].Remove(peer);
-
-        Volatile.Write(ref peerCellKeys[peer.Value], key);
-    }
-
-    public void Remove(PeerIndex peer)
-    {
-        long key = Volatile.Read(ref peerCellKeys[peer.Value]);
-        if (key == NO_CELL) return;
-
+    public void Remove(PeerIndex peer) =>
         Volatile.Write(ref peerCellKeys[peer.Value], NO_CELL);
 
-        if (!cells.TryGetValue(key, out HashSet<PeerIndex>? peers)) return;
-        if (!peers.Remove(peer)) return;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public long ReadCellKey(PeerIndex peer) =>
+        Volatile.Read(ref peerCellKeys[peer.Value]);
 
-        if (peers.Count == 0)
-            cells.Remove(key, out _);
-    }
-
-    public HashSet<PeerIndex>? GetPeers(Vector3 position) =>
-        cells.GetValueOrDefault(ComputeKey(position));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public long ComputeCellKey(Vector3 position) =>
+        ComputeKey(position);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private long ComputeKey(Vector3 position) =>

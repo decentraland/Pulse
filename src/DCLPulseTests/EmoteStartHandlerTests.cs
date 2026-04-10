6afe9ca2cb534148b0f5aa6c42031224c30e233f
Mikhail Agapov <mikhail.agapov@decentraland.org>
2026-04-10 12:49:12 +0300

    Fix Emote Seq related tests
    Signed-off-by: Mikhail Agapov <mikhail.agapov@decentraland.org>

--- src/DCLPulseTests/EmoteStartHandlerTests.cs
+++ src/DCLPulseTests/EmoteStartHandlerTests.cs
@@ -5,6 +5,7 @@ using Pulse;
 using Pulse.Messaging;
 using Pulse.Peers;
 using Pulse.Peers.Simulation;
+using System.Numerics;
 
 namespace DCLPulseTests;
 
@@ -14,6 +15,7 @@ public class EmoteStartHandlerTests
     private const uint MONOTONIC_TIME = 5000;
 
     private EmoteBoard emoteBoard;
+    private SnapshotBoard snapshotBoard;
     private ITimeProvider timeProvider;
     private EmoteStartHandler handler;
     private Dictionary<PeerIndex, PeerState> peers;
@@ -22,10 +24,11 @@ public class EmoteStartHandlerTests
     public void SetUp()
     {
         emoteBoard = new EmoteBoard(100);
+        snapshotBoard = new SnapshotBoard(100, 16);
         timeProvider = Substitute.For<ITimeProvider>();
         timeProvider.MonotonicTime.Returns(MONOTONIC_TIME);
 
-        handler = new EmoteStartHandler(emoteBoard, timeProvider, Substitute.For<ILogger<EmoteStartHandler>>());
+        handler = new EmoteStartHandler(emoteBoard, snapshotBoard, timeProvider, Substitute.For<ILogger<EmoteStartHandler>>());
         peers = new Dictionary<PeerIndex, PeerState>();
     }
 
@@ -83,5 +86,37 @@ public class EmoteStartHandlerTests
         handler.Handle(peers, peer, message);
 
         Assert.That(emoteBoard.Get(peer), Is.Null);
+    }
+
+    [Test]
+    public void Handle_BumpsSnapshotSeq_WhenSnapshotExists()
+    {
+        var peer = new PeerIndex(1);
+        peers[peer] = new PeerState(PeerConnectionState.AUTHENTICATED);
+
+        snapshotBoard.SetActive(peer);
+        var initial = new PeerSnapshot(Seq: 5, ServerTick: 4000, Parcel: 0, default(Vector3), default(Vector3), default(Vector3), 0f, 0, 0f, 0f, null, null, default(PlayerAnimationFlags), default(GlideState));
+        snapshotBoard.Publish(peer, initial);
+
+        var message = new ClientMessage { EmoteStart = new EmoteStart { EmoteId = "wave", DurationMs = 3000 } };
+
+        handler.Handle(peers, peer, message);
+
+        Assert.That(snapshotBoard.TryRead(peer, out PeerSnapshot updated), Is.True);
+        Assert.That(updated.Seq, Is.GreaterThan(initial.Seq));
+        Assert.That(updated.ServerTick, Is.EqualTo(MONOTONIC_TIME));
+    }
+
+    [Test]
+    public void Handle_DoesNotBumpSeq_WhenNoSnapshotExists()
+    {
+        var peer = new PeerIndex(1);
+        peers[peer] = new PeerState(PeerConnectionState.AUTHENTICATED);
+
+        var message = new ClientMessage { EmoteStart = new EmoteStart { EmoteId = "wave", DurationMs = 3000 } };
+
+        handler.Handle(peers, peer, message);
+
+        Assert.That(snapshotBoard.TryRead(peer, out _), Is.False);
     }
 }
